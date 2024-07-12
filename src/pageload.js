96212716bc6eee605c0f8e3e323420f0dc32e64f
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-07-12 13:39:44 -0700

    Start sketching out page load module

--- src/pageload.js
+++ src/pageload.js
@@ -1,4 +1,5 @@
 const pageLoad = () => {
+  const content = document.querySelector('#content');
   const header = document.createElement('div');
   const main = document.createElement('div');
   const footer = document.createElement('div');
@@ -9,11 +10,14 @@ const pageLoad = () => {
   main.classList.add('main');
   footer.classList.add('footer');
   title.classList.add('title');
+  title.textContent = 'Hope you brought your appetite!';
+  main.textContent = 'Content TBD';
+  footer.textContent = 'Content TBD';
 
-  document.appendChild(header);
+  content.appendChild(header);
   header.appendChild(title);
-  document.appendChild(main);
-  document.appendChild(footer);
+  content.appendChild(main);
+  content.appendChild(footer);
 }
 
 export default pageLoad;
