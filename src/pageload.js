f28c83fd2646bd84a617b2a7e324b8b736a7d3b2
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-07-16 17:12:25 -0700

    Tinker with DOM content

--- src/pageload.js
+++ src/pageload.js
@@ -1,23 +1,34 @@
+import restaurant from './restaurant.jpeg';
+
 const pageLoad = () => {
   const content = document.querySelector('#content');
-  const header = document.createElement('div');
+  const banner = document.createElement('div');
+  const container = document.createElement('div');
   const main = document.createElement('div');
+  const footContainer = document.createElement('div');
   const footer = document.createElement('div');
   const title = document.createElement('div');
+  const restaurantImage = new Image();
+  restaurantImage.src = restaurant;
 
 
-  header.classList.add('header');
-  main.classList.add('main');
-  footer.classList.add('footer');
+  banner.classList.add('banner');
   title.classList.add('title');
-  title.textContent = 'Hope you brought your appetite!';
-  main.textContent = 'Content TBD';
-  footer.textContent = 'Content TBD';
+  title.textContent = `Bob's Blobfish`;
+  container.classList.add('container');
+  main.classList.add('main');
+  restaurantImage.classList.add('restaurant');
+  footContainer.classList.add('footcontainer');
+  footer.classList.add('footer');
+  footer.textContent = 'Est. 1961';
 
-  content.appendChild(header);
-  header.appendChild(title);
-  content.appendChild(main);
-  content.appendChild(footer);
+  content.appendChild(banner);
+  banner.appendChild(title);
+  content.appendChild(container);
+  container.appendChild(main);
+  main.appendChild(restaurantImage);
+  content.appendChild(footContainer);
+  footContainer.appendChild(footer);
 }
 
 export default pageLoad;
