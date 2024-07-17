9daa6405a2e545b846f96871038504cee1645b5e
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-07-17 16:09:12 -0700

    Implement tabbed browsing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
-import pageLoad from './pageload.js';
+import homeLoad from './homeload.js';
+import menuLoad from './menuload.js';
+import aboutLoad from './aboutload.js';
 import './style.css';
 
-pageLoad();
+homeLoad();
+
+const homeBtn = document.querySelector('.home');
+const menuBtn = document.querySelector('.menu');
+const aboutBtn = document.querySelector('.about');
+
+const tabSwitch = (loadPage) => {
+  const content = document.getElementById('content');
+  while (content.firstChild) {
+  content.removeChild(content.lastChild);
+  }
+  loadPage();
+};
+
+homeBtn.addEventListener('click', () => {
+  tabSwitch(homeLoad);
+});
+
+menuBtn.addEventListener('click', () => {
+  tabSwitch(menuLoad);
+});
+
+aboutBtn.addEventListener('click', () => {
+  tabSwitch(aboutLoad);
+});
