c68bdec23df031fe6df1f85f29e75999be42f882
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-07-18 11:45:11 -0700

    Begin fleshing out menu page

--- src/menuload.js
+++ src/menuload.js
@@ -1,3 +1,43 @@
-const menuLoad = () => {};
+import menuJson from './menu.json5';
+
+const menuLoad = () => {
+  const content = document.getElementById('content');
+  const menuContainer = document.createElement('div');
+  const menu = document.createElement('div');
+
+  menuContainer.classList.add('menucontainer');
+  menu.classList.add('menu');
+  content.appendChild(menuContainer);
+  
+  const populateMenu = obj => {
+    Object.keys(obj).forEach(key => {
+      if (typeof obj[key] === 'object') {
+
+        const item = document.createElement('div');
+        const name = document.createElement('div');
+        const desc = document.createElement('div');
+        const price = document.createElement('div');
+        const imageContainer = document.createElement('div');
+        const image = new Image();
+
+        item.classList.add('menuitem');
+        name.textContent = obj[key].name;
+        desc.textContent = obj[key].description;
+        price.textContent = obj[key].price;
+        image.src = `./${obj[key].image}`;
+      
+        menu.appendChild(item);
+        item.appendChild(name);
+        item.appendChild(desc);
+        item.appendChild(price);
+        item.appendChild(imageContainer);
+      }
+    })
+  }
+
+  populateMenu(menuJson);
+
+  menuContainer.appendChild(menu);
+};
 
 export default menuLoad;
