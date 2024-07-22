2a11cd7a96b83ffe31d0d6121c35b240a358703a
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-07-22 10:19:55 -0700

    Change json5 data file to js for ease of exporting

---
+++ src/menu.js
@@ -0,0 +1,50 @@
+export default {
+  item1: {
+    name: 'Buffalo Blowfish Tails',
+    description: 'Crispy blowfish tails tossed in a tangy buffalo sauce. A unique and flavorful appetizer that pairs well with a cold beer.',
+    price: '$12.99',
+    image: require('./images/blowfish-tails.jpeg'),
+  },
+  item2: {
+    name: 'Clam Chowder Dumplings',
+    description: 'Steamed dumplings filled with New England clam chowder—creamy, chunky, and bursting with clam flavor. Accompanied by a chive dipping sauce.',
+    price: '$11.75 (6 dumplings)',
+    image: require('./images/clam-chowder-dumplings.jpeg'),
+  },
+  item3: {
+    name: 'Shrimp-Stuffed Avocado Boat',
+    description: 'Half an avocado filled with shrimp and citrus segments, topped with a zesty cilantro-lime dressing. Served chilled.',
+    price: '$13.99',
+    image: require('./images/shrimp-stuffed-avocado.jpeg'),
+  },
+  item4: {
+    name: 'Seaweed-Wrapped Tuna Tacos',
+    description: 'Crispy seaweed shells filled with diced ahi tuna, avocado, mango salsa, and a wasabi aioli drizzle. A fusion of Japanese and Mexican flavors.',
+    price: '$14.95 (3 tacos)',
+    image: require('./images/seaweed-tuna-tacos.jpeg'),
+  },
+  item5: {
+    name: 'Swordfish Pastrami Sandwich',
+    description: 'Thinly sliced swordfish pastrami layered on rye bread with pickled red onions, Dijon mustard, and arugula. Served with a side of house-made potato chips.',
+    price: '$15.95',
+    image: require('./images/swordfish-sandwich.jpeg'),
+  },
+  item6: {
+    name: 'Oyster Platter',
+    description: 'Raw West Coast oysters dressed with watercress granita, celeriac, and olive oil.',
+    price: '$18.50 (half dozen)',
+    image: require('./images/oyster-platter.jpeg'),
+  },
+  item7: {
+    name: 'Miso-Glazed Black Cod',
+    description: 'Buttery black cod fillet marinated in a sweet and savory miso glaze, then broiled until caramelized. Served with steamed bok choy.',
+    price: '$22.95',
+    image: require('./images/black-cod.jpeg'),
+  },
+  item8: {
+    name: 'Salmon Wellington',
+    description: 'Flaky puff pastry encases a center-cut salmon fillet, spinach, and dill cream cheese. Baked until golden brown and served with a lemon beurre blanc.',
+    price: '$23.50',
+    image: require('./images/salmon-wellington.jpeg'),
+  },
+}
