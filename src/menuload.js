01972f51c121d96980646ffc04be8005cb85581c
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-07-22 11:23:50 -0700

    Add a title to the menu page

--- src/menuload.js
+++ src/menuload.js
@@ -1,19 +1,24 @@
-import menuJson from './menu.json5';
+import menuJson from './menu.js';
 
 const menuLoad = () => {
   const content = document.getElementById('content');
   const menuContainer = document.createElement('div');
+  const title = document.createElement('div');
   const menu = document.createElement('div');
 
   menuContainer.classList.add('menucontainer');
+  title.classList.add('menutitle');
+  title.textContent = 'Menu';
   menu.classList.add('menu');
-  content.appendChild(menuContainer);
+
+  menuContainer.appendChild(title);
   
   const populateMenu = obj => {
     Object.keys(obj).forEach(key => {
       if (typeof obj[key] === 'object') {
 
         const item = document.createElement('div');
+        const info = document.createElement('div');
         const name = document.createElement('div');
         const desc = document.createElement('div');
         const price = document.createElement('div');
@@ -21,16 +26,23 @@ const menuLoad = () => {
         const image = new Image();
 
         item.classList.add('menuitem');
+        info.classList.add('info');
+        image.classList.add('dishimage');
+        name.classList.add('name');
+        desc.classList.add('desc');
+        price.classList.add('price');
         name.textContent = obj[key].name;
         desc.textContent = obj[key].description;
         price.textContent = obj[key].price;
-        image.src = `./${obj[key].image}`;
+        image.src = obj[key].image;
       
         menu.appendChild(item);
-        item.appendChild(name);
-        item.appendChild(desc);
-        item.appendChild(price);
+        item.appendChild(info);
+        info.appendChild(name);
+        info.appendChild(desc);
+        info.appendChild(price);
         item.appendChild(imageContainer);
+        imageContainer.appendChild(image);
       }
     })
   }
@@ -38,6 +50,7 @@ const menuLoad = () => {
   populateMenu(menuJson);
 
   menuContainer.appendChild(menu);
+  content.appendChild(menuContainer);
 };
 
 export default menuLoad;
